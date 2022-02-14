034c87cf75a091ffa09557190932c0cce9d5ead0
Vertexi <778950965@qq.com>
2022-02-15 05:19:13 +0800

    add motion

--- tetris.py
+++ tetris.py
@@ -1,4 +1,5 @@
 import random
+import pprint
 
 tetromino_I_1 = [[0, 0, 0, 0],
                  [0, 0, 0, 0],
@@ -79,17 +80,112 @@ tetrominos = [tetromino_I, tetromino_L, tetromino_J, tetromino_S,
 
 class GameMap:
     def __init__(self):
-        self.rows = 20
-        self.cols = 10
+        self.rows = 22
+        self.cols = 12
+        self.game_map = None
+        self.tetromino = None
+
+        self.update_map = self.iter_tetromino_area(self.update_map_action)
+        self.collide_detect = \
+            self.iter_tetromino_area(self.collide_detect_action)
+
+        self.game_over = False
+
+    def get_tetromino_area(self):
+        window_x0 = self.tetromino.pos_x
+        window_y0 = self.tetromino.pos_y
+        tetromino_len = self.tetromino.length
+        window_x1 = window_x0 + tetromino_len
+        window_y1 = window_y0 - tetromino_len
+        if window_y1 < 0:
+            window_y1 = -1
+        return window_x0, window_x1, window_y0, window_y1
+
+    def iter_tetromino_area(self, action):
+        def func():
+            window_x0, window_x1, window_y0, window_y1 = self.get_tetromino_area()
+            print(window_x0, window_x1, window_y0, window_y1)
+            tetromino_x = 0
+            for i in range(window_x0, window_x1):
+                tetromino_y = self.tetromino.length - 1
+                for j in range(window_y0, window_y1, -1):
+                    if action(j, i, tetromino_y, tetromino_x):
+                        return True
+                    tetromino_y -= 1
+                tetromino_x += 1
+            return False
+
+        return func
+
+    def update_map_action(self, j, i, tetromino_y, tetromino_x):
+        self.game_map[j][i] += \
+            self.tetromino.tetromino[tetromino_y][tetromino_x]
+
+    def collide_detect_action(self, j, i, tetromino_y, tetromino_x):
+        if (self.game_map[j][i] +
+                self.tetromino.tetromino[tetromino_y][tetromino_x]) > 1:
+            return True
+
+    def move_down(self):
+        self.tetromino.pos_y += 1
+        if self.collide_detect():
+            self.tetromino.pos_y -= 1
+            self.update_map()
+            self.add_tetromino()
+            self.print_game()
+
+    def move_right(self):
+        self.tetromino.pos_x += 1
+        if self.collide_detect():
+            self.tetromino.pos_x -= 1
+
+    def move_left(self):
+        self.tetromino.pos_x -= 1
+        if self.collide_detect():
+            self.tetromino.pos_x += 1
+
+    def add_tetromino(self):
+        self.tetromino = Tetromino(self.cols)
+        if self.collide_detect():
+            self.game_over = True
+
+    def init_map(self):
         self.game_map = \
             [[0 for col in range(self.cols)] for row in range(self.rows)]
+        # draw wall
+        for i in range(self.rows):
+            self.game_map[i][0] = 1
+            self.game_map[i][-1] = 1
+        for j in range(self.cols):
+            self.game_map[-2][j] = 1
+            self.game_map[-1][j] = 1
+
+    def init_game(self):
+        self.init_map()
+        self.add_tetromino()
+
+    def run(self):
+        self.init_game()
+        while not self.game_over:
+            self.move_down()
+
+    def print_game(self):
+        pprint.pprint(self.game_map)
 
 
 class Tetromino:
-    def __init__(self):
-        self.tetromino_type = random.randint(0, 7 - 1)
-        self.orient = random.randint(1, 4)
-        self.tetromino = None
+    def __init__(self, map_cols: int):
+        self.tetromino_type = random.randint(0, len(tetrominos) - 1)
+        self.orient = 0
+        self.tetromino = tetrominos[self.tetromino_type][self.orient]
+        self.length = len(self.tetromino)
+        self.pos_x = random.randint(1, map_cols - self.length - 1)
+        self.pos_y = 1
 
-    def shape(self):
-        self.tetromino = tetrominos[self.orient]
+    def rotate(self):
+        self.orient += 1
+        self.tetromino = tetrominos[self.tetromino_type][self.orient]
+
+
+game = GameMap()
+game.run()
